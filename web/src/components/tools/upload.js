b506e99e56ad42b4876a5362b51c36b72f79f4e6
Tomaz Korenika <tomazkorenika@Tomazs-Mac-mini.local>
2021-08-02 17:02:46 +0200

    Fix some ui issues

--- web/src/components/tools/upload.js
+++ web/src/components/tools/upload.js
@@ -1,36 +1,65 @@
 import React, { useState } from "react";
 
-import { Button, AtSignIcon } from "@chakra-ui/react";
+import { 
+  Button, 
+  AtSignIcon,   
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, 
+  useDisclosure
+} from "@chakra-ui/react";
 
 import { uploadVMA } from "../../api/api";
 
-export const Upload = ({ path }) => {
+const UploadVMAPresent = ({ loading, onChangeHandler, entity }) =>  {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button colorScheme="orange" variant="solid" onClick={onOpen}>
+        Upload
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Upload a new {entity} </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <input type="file" name="file" onChange={onChangeHandler} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose} isLoading={loading}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export const Upload = ({ name }) => {
   const [isFilePicked, setIsFilePicked] = useState(false);
   const [loading, setIsLoading] = useState(false);
 
   const changeHandler = (event) => {
-    setIsFilePicked(true);
+		setIsFilePicked(true);
     setIsLoading(true);
     uploadFile(event.target.files[0]);
-  };
+	};
 
   const uploadFile = async (file) => {
     const data = new FormData();
     data.append('file', file);
     data.append('name', file.name);
-    const upload = await uploadVMA(data, 'vma');
+    const upload = await uploadVMA(data, name);
     setIsLoading(false);
   }
 
-  return <UploadVMAPresent loading={loading} onChangeHandler={changeHandler} />
+  return <UploadVMAPresent onChangeHandler={changeHandler} entity={name} />
 }
 
-const UploadVMAPresent = ({ loading, onClick, onChangeHandler }) => {
-  return (
-    <>
-      <input type="file" name="file" onChange={onChangeHandler} />
-      <Button colorScheme="orange" variant="solid" isLoading={loading} >
-      </Button>
-    </>
-  )
-}
